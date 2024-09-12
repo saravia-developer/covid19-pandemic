fa0104881db4b5af84247bf26558b916ebf65745
Luis Saravia <saraviatornero310@gmail.com>
2024-09-12 17:23:44 -0500

    fix(component-style): Fixed Import

--- src/components/todayRegister/TodayRegister.jsx
+++ src/components/todayRegister/TodayRegister.jsx
@@ -1,16 +1,88 @@
-import React, { useEffect, useState } from "react";
-import style from "./TodayRegister.module.scss";
+import React from "react";
+import style from "./todayRegister.module.scss";
 
 export default function TodayRegister({ lastRegister }) {
-  const { death, hospitalizedCurrently, inIcuCurrently } = lastRegister;
+  const {
+    positive,
+    death,
+    hospitalizedCurrently,
+    inIcuCurrently,
+    onVentilatorCurrently,
+    deathIncrease,
+    positiveIncrease,
+    totalTestResults,
+    totalTestResultsIncrease,
+  } = lastRegister;
 
   return (
     <div className={style.todayRegister}>
       <div className={style.currentlyCasesCovid19}>
-        <h3>Covid-19 until March 7, 2021</h3>
-        <div>
+        <h3>Data for March 7, 2021</h3>
+
+        <div className={""}>
+          <h3>Cases</h3>
+
+          <div>
+            <strong>Total cases</strong>
+            <b>{positive}</b>
+          </div>
+
+          <div>
+            <strong>New cases today</strong>
+            <b>{positiveIncrease}</b>
+          </div>
+        </div>
+
+        <div className={""}>
+          <h3>Tests</h3>
+
+          <div>
+            <strong>Total test</strong>
+            <b>{totalTestResults}</b>
+          </div>
+
+          <div>
+            <strong>New tests today</strong>
+            <b>{totalTestResultsIncrease}</b>
+          </div>
+        </div>
+
+        <div className={""}>
+          <h3>Tests</h3>
+
+          <div>
+            <strong>Total test</strong>
+            <b>{totalTestResults}</b>
+          </div>
+
+          <div>
+            <strong>New tests today</strong>
+            <b>{totalTestResultsIncrease}</b>
+          </div>
+        </div>
+
+        <div className={""}>
+          <h3>Hospitalization</h3>
+
+          <div>
+            <strong>Currently hospitalized</strong>
+            <b>{hospitalizedCurrently}</b>
+          </div>
+
+          <div>
+            <strong>Currently in ICU</strong>
+            <b>{inIcuCurrently}</b>
+          </div>
+
+          <div>
+            <strong>Currently on ventilator</strong>
+            <b>{onVentilatorCurrently}</b>
+          </div>
+        </div>
+
+        {/* <div>
           <div className={style.dataField}>
-            <b>HospitalizedCurrently</b>
+            <b>Hospitalized Currently</b>
             <b>{hospitalizedCurrently}</b>
           </div>
 
@@ -20,10 +92,15 @@ export default function TodayRegister({ lastRegister }) {
           </div>
 
           <div className={style.dataField}>
+            <b>Death Increase</b>
+            <b>{deathIncrease}</b>
+          </div>
+
+          <div className={style.dataField}>
             <b>Death</b>
             <b>{death}</b>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
